856d59c5fc27c9d89258a31335070334432effb6
yoonsun <jyoonsun0217@gmail.com>
2022-01-28 02:09:55 +0900

    IndexError case

--- algorithm/2309.py
+++ algorithm/2309.py
@@ -1,24 +1,24 @@
-# input 9개
-# 오름차순으로 정렬
-# 9개의 수 중 7개의 수를 더했을 때 100이 되는 경우
-# 7개의 수를 오름차순 정렬 후 출력
-# map(int, input(), split())
-# input을 어떻게 받을지는 잘 모르겠으니 중간 로직을 짜자
-dwarfs = []
-while sum == 
+# '''
+# 난쟁이..이놈의 난쟁이들..
+# 9명 중에서 7명이 진짜 난쟁이고
+# 2명은 이상한 놈들인데..
+# 7명 난쟁이 키의 합이 100이라고 했으니
+# 9명 키 합에서 2명 키 합을 뺐을 때 100이 나온다면
+# 나머지 7명이 진짜 난쟁이겠구나
+# '''
 
-# a, b, c, d, e, f, g, h, i = input().split('\n')
-# a = int(a)
-# b = int(b)
-# c = int(c)
-# d = int(d)
-# e = int(e)
-# f = int(f)
-# g = int(g)
-# h = int(h)
-# i = int(i)
 
-# dwarfs = list(input())
+dwarfs = [int(input()) for _ in range(9)]
+dwarfs.sort()
+cnt = sum(dwarfs)
+for i in range(9):
+    for j in range(i+1, 9):
+        if cnt - (dwarfs[j] + dwarfs[i]) == 100: #IndexError: list assignment index out of range
+            del dwarfs[j]
+            del dwarfs[i]
+            
+            print(dwarfs)
+    
 
-# sorted(dwarfs)
-# print(dwarfs)
+
+
