233804129cb73e599fbe73b14dc015994bf26a35
yoonsun <jyoonsun0217@gmail.com>
2022-01-29 01:05:02 +0900

    2309

--- algorithm/2309.py
+++ algorithm/2309.py
@@ -7,26 +7,64 @@
 # 나머지 7명이 진짜 난쟁이겠구나
 # '''
 
+# dwarfs = []
+# for _ in range(9):
+#     dwarfs.append(int(input()))
+
+# for i in range(9):
+#     for j in range(i+1, 9):
+#         if sum(dwarfs) - (dwarfs[i] + dwarfs[j]):
+#             num1 = dwarfs[i]
+#             num2 = dwarfs[j]
+
 
 dwarfs = [int(input()) for _ in range(9)]
+
 dwarfs.sort()
 cnt = sum(dwarfs)
+num_1 = 0
+num_2 = 0
+
 for i in range(9):
-    for j in range(9):
-        if dwarfs[i] == dwarfs[j]:
-            continue
+    for j in range(i+1, 9):
+        if cnt - (dwarfs[i] + dwarfs[j]) == 100:
+            num_1, num_2 = dwarfs[i], dwarfs[j]
 
-        elif cnt - (dwarfs[i] + dwarfs[j]) == 100: #IndexError: list assignment index out of range
+            break
+
+dwarfs.remove(num_1)
+dwarfs.remove(num_2)
+
+for i in dwarfs:
+    print(i)
+
+
+
+# dwarfs = []
+# for _ in range(9):
+#     dwarfs.append(int(input()))
+
+# dwarfs.sort()
+# cnt = sum(dwarfs)
+# for i in range(len(dwarfs)):
+#     for j in range(len(dwarfs)):
+#         if dwarfs[i] == dwarfs[j]:
+#             continue
+
+#         elif cnt - (dwarfs[i] + dwarfs[j]) == 100: #IndexError: list assignment index out of range
             
-            del dwarfs[j]
-            del dwarfs[i]
+#             del dwarfs[j]
+#             del dwarfs[i]
 
-    if len(dwarfs) == 7:
-        break
-            
+#     if len(dwarfs) == 7:
+#         break
 
 
-print(dwarfs)
+# for dwarf in dwarfs:
+#     print(dwarf)
+
+
+
     
 
 
