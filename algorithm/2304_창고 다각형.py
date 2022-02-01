80dc8df25ad0d09a4ad06b7a03b8f31bbdef321e
yoonsun <jyoonsun0217@gmail.com>
2022-02-01 20:58:49 +0900

    mod

--- algorithm/2304_창고 다각형.py
+++ algorithm/2304_창고 다각형.py
@@ -7,11 +7,11 @@ for _ in range(N):
     length.append(L)
     height.append(H)
 
-    # 가로길이: length (max - min) + 1
-    l = (max(length) - min(length)) + 1
+    # # 가로길이: length (max - min) + 1
+    # l = (max(length) - min(length)) + 1
 
-    # 최대 면적
-    max_width = l * max(height)
+    # # 최대 면적
+    # max_width = l * max(height)
 
     # length 오름차순으로 이차원 리스트 요소 정렬
     while len(length) != 0:
@@ -32,11 +32,11 @@ for _ in range(N):
     # right_area = 0 
     
     # 넓이 면적 구하는데에 필요한 기둥에 대한 원소들만 받을 리스트
-    pillars = list(width[0],width[-1])
+    pillars = [width[0],width[-1]]
 
     # 가장 높은 기둥 찾기
     for i in range(N):
-        if width[i][1] is max(height):
+        if width[i][1] == max(width[i][1]):
             pillars.append(width[i])
 
         # 왼쪽 기둥에서 가장 높은 기둥까지
@@ -51,70 +51,21 @@ for _ in range(N):
             if width[-1][1] <= width[k][1]:
                 pillars.append(width[k])
 
+    # 기둥 위치로 정렬, 최종 면적 값 설정
     pillars = sorted(pillars)
+    rlt = 0
+    for p in range(len(pillars)):
+        if pillars[p][1] <= pillars[p+1][1]:
+            rlt += pillars[p][1] * (pillars[p+1][0] - pillars[p][0])
 
+        elif pillars[p][1] == max(pillars[p][1]):
+            rlt += pillars[p][1]
 
-    # 가장자리 두개의 기둥은 무조건 포함
-    # right_pillar = width[N] or width[-1]
+        elif max(pillars[p][1]) > pillars[p+1][1]:
+            rlt += pillars[p+1][1] * (pillars[p+1][0] - pillars[p][0])
 
+            if pillars[p] == pillars(len(pillars)):
+                break
 
-
-
-
-    # # 첫번째 기둥과 맨 마지막 기둥은 필수로 포함되어야 함
-    # # 가장 높은 기둥의 넓이 먼저 더하기
-    # for i in range(N):
-    #     if width[i][1] is max(height):
-    #         center_area += width[i][1]
-
-    # j = 0
-    # while j == i:
-    #     if width[j][1] <= width[j+1][1]:
-    #         left_area += width[j][1] * (width[j+1][0] - width[j][0])
-    #         j += 1
-
-            
-
-
-
-    for j in range(i):
-        if width[j][1] <= width[j+1][1]:
-            left_area += width[j][1] * (width[j+1][0] - width[j][0])
-
-        elif width[j][1] > width[j+1][1]:
-            
-        
-            
-
-        if width[i][0] < width[i+1][0]:
-            total_area += width[i][0] * (width[i+1][0] - width[i][0])
-
-
-
-    for i in range(N):
-        if width[i][0] < width
-
-
-
-
-
-
- 
-
-
-# for i in range(N):
-#     if length[i] is min(length):
-#         width.append([length[i], height[i]])
-
-
-
-
-
-
-
-# # 가로길이: length (max - min) + 1
-# l = (max(length) - min(length)) + 1
-
-# # 최대 면적
-# max_width = l * max(height)
+    print(rlt)
 
