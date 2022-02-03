d7f9475d9f0aa51040cf8835f862c71dd18ef399
yoonsun <jyoonsun0217@gmail.com>
2022-02-03 13:56:21 +0900

    update

--- algorithm/2304_창고 다각형.py
+++ algorithm/2304_창고 다각형.py
@@ -1,7 +1,134 @@
-N = int(input()) # 기둥 개수
-length = [] # 기둥 왼쪽 면 위치 L list
-height = [] # 기둥 높이 H list
-width = [] # L, H 이차원 list
+# # N = int(input())
+# # length = []
+# # height = []
+# # width = []
+
+# # for _ in range(N):
+# #     L, H = map(int, input().split())
+# #     length.append(L)
+# #     height.append(H)
+
+# #     while len(length) != 0:
+# #         i = 0
+# #         if length[i] is min(length):
+# #             width.append([length[i], height[i]])
+# #             del length[i]
+# #             del height[i]
+
+# #         else:
+# #             i += 1
+
+# #     width = sorted(width)
+
+# # for j in range(len(width)):
+# #     length.append(width[j][0])
+# #     height.append(width[j][1])
+
+# # result = []
+
+# # for k in range(N):
+# #     while height[k] is not max(height):
+# #         result.append(height[k] * (length[k+1] - length[k]))
+# #         if height[k] > height[k+1]:
+# #             height[k+1] = height[k]
+
+        
+# from turtle import heading
+
+
+# N = int(input())
+# length = []
+# height = []
+# width = []
+
+# for _ in range(N):
+#     L, H = map(int, input().split())
+#     length.append(L)
+#     height.append(H)
+
+#     while len(length) != 0:
+#         i = 0
+#         if length[i] is min(length):
+#             width.append([length[i], height[i]])
+#             del length[i]
+#             del height[i]
+
+#         else:
+#             i += 1
+
+#     width = sorted(width)
+
+# for j in range(len(width)):
+#     length.append(width[j][0])
+#     height.append(width[j][1])
+
+# max_H = 1 # 최소 높이 1
+# max_L = 0 # 최소 시작지점 0
+
+# # 최고 높이 기둥을 기준으로
+# if height[j] is max(height):
+#     max_H = height[j]
+#     max_L = length[j]
+
+# left = []
+# right = [] 
+
+# # 왼쪽 => 0부터 최고 기둥까지
+# for k in range(N):
+#     if height[k] is max(height):
+#         break
+
+#     if height[k] > height[k+1]:
+#         height[k+1] = height[k]
+    
+#     left.append(height[k] * (length[k+1] - length[k]))
+
+# for k in range(N-1, 0, -1):
+#     if height[k] is max(height):
+#         break
+
+#     if height[k-1] < height[k]:
+#         height[k-1] = height[k]
+
+#     right.append(height[k] * (length[k] - length[k-1]))
+
+# rlt = sum(left) + sum(right) + max(height)
+
+# print(rlt)
+        
+# # for k in range
+
+# # for k in range(max(length)+1):
+# #     if left < height[k]:
+# #         left = height[k]
+# #     total += left
+
+# # # 오른쪽 => reverse idx로 맨 끝부터 최고 기둥까지
+# # for k in range(max(length), max_L, -1):
+# #     if right < height[k]:
+# #         right = height[k]
+# #     total += right
+
+# # print(total)
+
+# # for k in range(max_L+1):
+# #     if left < height[k]:
+# #         left = height[k]
+# #     total += left
+
+# # for k in range(length[N], max_L, -1):
+# #     if right < height[k]:
+# #         right = height[k]
+# #     total += right
+
+# # print(total)
+
+
+
+N = int(input())
+length = []
+height = []
+width = []
 
 for _ in range(N):
     L, H = map(int, input().split())
@@ -18,69 +145,103 @@ for _ in range(N):
         else:
             i += 1
 
-    width = sorted(width) # L 기준으로 오름차순 정렬
+    width = sorted(width)
 
-# print(width) => L 기준 오름차순 정렬
-# length, height 리스트는 다시 빈 리스트가 됨
-# 이제 다시 width에 넣은 순서대로 length, height에 요소 넣어주기
+# print(width)
 
-# 기둥 위치 순서대로 정렬된 length, heigth list
+# 위치 기반으로 정렬된 length, height list
 for j in range(len(width)):
     length.append(width[j][0])
     height.append(width[j][1])
 
+# 가장 높은 기둥을 중심으로 왼쪽, 오른쪽 면적 구하기
+center_idx = 0
+total = 0
 # print(length)
 # print(height)
 
-
-left_area = []
-right_area = []
-center_area = []
-
-
-for k in range(N):
-    if height[k] is max(height):
-        center_area.append(height[k])
-
-        # center_area += height[k]
-
-
-# print(center_area)
-
-# 왼쪽 기둥부터 max 기둥전까지
-
-for l in range(N):
-    if height[l] is max(height):
+for i in range(len(height)):
+    if height[i] is max(height):
+        center_idx = i
         break
 
-    if height[l] <= height[l+1]:
-        left_area.append(height[l] * (length[l+1] - length[l]))
+total += height[center_idx]
 
-    else:
-        left_area.append(height[l] * (length[l+1] - length[l]))
-        height[l+1] = height[l]
+# print(center_idx)
+# print(height[center_idx])
+# print(total)
 
-# print(left_area)
+# 왼쪽 면적 구하기 전 정리
+for i in range(center_idx):
+    if height[i] >= height[i+1]:
+        height[i+1] = height[i]
+    total += height[i] * (length[i+1] - length[i])
 
-# length, height list에서 왼쪽 idx 제거 후 역순 sort (오른쪽 기둥부터 max 기둥 계산 위해)
+# 오른쪽 면적 reverse idx 사용해서 정리
+for i in range(len(height)-1, center_idx, -1):
+    if height[i] >= height[i-1]:
+        height[i-1] = height[i]
+    total += height[i] * (length[i] - length[i-1])
+
+print(total)
 
 
-# 오른쪽 기둥부터 max 기둥전까지 reverse idx
-for r in range(N-1, 0, -1):
-    if height[r] is max(height):
-        break
 
-    if height[r] <= height[r-1]:
-        right_area.append(height[r] * (length[r] - length[r-1]))
+# # 최종 면적 값 받을 total값 초기화
+# total = 0
 
-    else:
-        right_area.append(height[r] * (length[r] - length[r-1]))
-        height[r-1] = height[r]
+# # 왼쪽 면적 합산
+# for i in range(center_idx):
+#     total += height[i] * (length[i+1] - length[i])
 
-# print(right_area)
-        
-    rlt = sum(center_area) + sum(right_area) + sum(left_area)
-print(rlt)
+# # 오른쪽 면적 합산 reverse idx
+# for i in range(len(height)-1, center_idx, -1):
+#     total += height[i] * (length[i] - length[i-1])
 
+# # center pillar 값을 더해주면서 최종 면적 정리
+# total += height[center_idx]
+
+# print(total)
+
+
+
+
+
+
+# ################################
+# # max_L = 0 # 최소 시작지점 0
+# # # 최고 높이 기둥을 기준으로
+# # if height[j] is max(height):
+# #     max_L = length[j]
+
+ 
+# # # 왼쪽 => 0부터 최고 기둥까지
+# # total = 0
+# # left = 0
+# # for k in range(max_L+1):
+# #     if height[k] is max(height):
+# #         break
+
+# #     if height[k] > left:
+# #         left = height[k]
+    
+# #     total += left
+# #     #left.append(height[k] * (length[k+1] - length[k]))
+
+# # right = 0
+# # for k in range(max(length), 0, -1):
+# #     if height[k] is max(height):
+# #         break
+
+# #     if right < height[k]:
+# #         right = height[k]
+    
+# #     total += right
+# #     #right.append(height[k] * (length[k] - length[k-1]))
+
+# # # rlt = sum(left) + sum(right) + max(height)
+# # print(total)    
+
+# # #print(rlt)
 
 
---
+++ algorithm/cccc.py
@@ -0,0 +1,9 @@
+N = int(input())
+
+width = []
+length = []
+height = []
+
+for i in range(N):
+    L, H = map(int, input().split())
+    
