bcd313e473e8812c50533adf99aa97c5bf70f3e4
yoonsun <jyoonsun0217@gmail.com>
2022-02-09 16:55:01 +0900

    mod

---
+++ algorithm/challange/sol1.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.stdin = open('input.txt')
+#
+# T = int(input())
+#
+#
+# for tc in range(1, T + 1):
+#
+#     print(f'#{tc} ')
+#
+
+a, b = map(int, input().split())
+c = int(input())
+
+print(a, b)
+print(c)
