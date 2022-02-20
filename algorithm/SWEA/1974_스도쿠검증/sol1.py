c93f6ded2db3db6e39f71f966f3b9c5bf1820f6f
yoonsun <jyoonsun0217@gmail.com>
2022-02-20 23:28:11 +0900

    mod

--- algorithm/SWEA/1974_스도쿠검증/sol1.py
+++ algorithm/SWEA/1974_스도쿠검증/sol1.py
@@ -11,9 +11,53 @@ sys.stdin = open('sudoku.txt')
 
 T = int(input())
 
-numb_lst = [x for x in range(1, 10)]
 
 for tc in range(1, T + 1):
     matrix = [list(map(int, input().split())) for _ in range(9)]
+    rlt_1 = rlt_2 = rlt_3 = 1
+
+    # grid matrix check
+    for r in range(0, 9, 3):
+        for c in range(0, 9, 3):
+            grid_lst = [0] * 10
+            for dr in range(3):
+                for dc in range(3):
+                    grid_lst[matrix[r+dr][c+dc]] += 1
+
+            for i in range(1, 10): # range 설정 조심~!
+                if grid_lst[i] != 1:
+                    rlt_1 *= 0
+                    break
+
+    # row matrix check
+    # col matrix check
+    for r in range(9):
+        row_lst = [0] * 10
+        col_lst = [0] * 10
+        for c in range(9):
+            row_lst[matrix[r][c]] += 1
+            col_lst[matrix[c][r]] += 1
+
+        for i in range(1, 10):
+            if row_lst[i] != 1:
+                rlt_2 *= 0
+                break
+            if col_lst[i] != 1:
+                rlt_3 *= 0
+                break
+
+    print(f'#{tc} {rlt_1 * rlt_2 * rlt_3}')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- algorithm/SWEA/5432_쇠막대기자르기/sol1.py
+++ algorithm/SWEA/5432_쇠막대기자르기/sol1.py
@@ -12,18 +12,19 @@ sys.stdin = open('iron.txt')
 
 T = int(input())
 for tc in range(1, T + 1):
-    lst = list(input())
-    iron = 0 # 쌓여있는 쇠막대기 개수
-    cnt = 0 # 잘린 쇠막대기 개수
+    lst = input()
+    cnt = rlt = 0
 
-    i = 0
-    while True:
-        # 종료조건 설정
-        if i == len(lst):
-            break
+    for i in range(len(lst)):
+        if lst[i] == '(': # 열린 괄호
+            cnt += 1 # 막대기 개수 +1 누적해주기
+        else: # 닫힌 괄호
+            if lst[i-1] == '(': # 그 전에 열린괄호가 나온 경우라면 레이저이므로
+                cnt -= 1
+                rlt += cnt # 막대기 개수 -1 해주고 누적 막대기 개수 rlt 더해주기
+            else: # ')' 가 나왔다면 막대기가 끝났다는 표시이므로
+                cnt -= 1
+                rlt += 1 # 막대기 하나 끝날 때마다 누적되는 막대기 수 +1
 
-    # i = 0
-    # while i < len(lst): # input 개수보다 i가 작은 동안 while문 반복
+    print(f'#{tc} {rlt}')
 
-
-    print(f'#{tc} ')
