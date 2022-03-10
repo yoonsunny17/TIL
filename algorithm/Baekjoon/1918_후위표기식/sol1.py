4e7e68dee975abab501217104f44e0d619958ad2
yoonsun <jyoonsun0217@gmail.com>
2022-03-10 22:53:03 +0900

    stack pr

--- algorithm/Baekjoon/1918_후위표기식/sol1.py
+++ algorithm/Baekjoon/1918_후위표기식/sol1.py
@@ -13,10 +13,12 @@ for code in codes:
             check.append(temp)
             temp = memo.pop()
 
-    elif code == '*':  # 곱하기인 경우에는,
-        memo.append(code)  # stack에 저장해줘 (icp == 2)
+    elif code == '*' or code == '/':  # 곱하기 또는 나누기라면,
+        while memo and (memo[-1] == '*' or memo[-1] == '/'): # stack이 비어있지 않고, 우선순위가 동등한 애들이라면
+            check.append(memo.pop()) # 연산자 꺼내서 check에 넣어줘
+        memo.append(code)  # 그리고 stack에 저장해줘 (icp == 2)
 
-    elif code == '+':  # 더하기인 경우에는,
+    elif code == '+' or code == '-':  # 더하기인 경우에는,
         while memo and memo[-1] != '(':  # stack이 비어있지 않고, 열린괄호 나오기 전까지는
             check.append(memo.pop())  # 연산자 꺼내서 check에 넣어줘
         memo.append(code)
@@ -24,36 +26,11 @@ for code in codes:
     else:  # 피연산자인 경우에는,
         check.append(code)  # check에 바로 저장해줘
 
+# memo 에 남아있는 연산자들을 다 pop 해주자
+if len(memo) != 0:
+    while True:
+        if len(memo) == 0:
+            break
+        check.append(memo.pop())
+
 print(''.join(check))
-
-
-# codes = input()
-# rlt = list() # 최종 출력 list
-# stack = [] # 연산자 모아둘 empty stack
-#
-# for code in codes:
-#     if code.isalpha():
-#         rlt.append(code)
-#     else:
-#         if code == '(':
-#             stack.append('(')
-#         elif code == '+':
-#             stack.append('+')
-#         elif code == '-':
-#             stack.append('-')
-#         elif code == '*':
-#             stack.append('*')
-#         elif code == '/':
-#             stack.append('/')
-#         elif code == ')':
-#             while True:
-#                 if stack[-1] == '(':
-#                     stack.pop()
-#                     break
-#                 rlt.append(stack.pop())
-#
-# while True:
-#     if not len(stack):
-#         print(''.join(rlt))
-#         break
-#     rlt.append(stack.pop())
